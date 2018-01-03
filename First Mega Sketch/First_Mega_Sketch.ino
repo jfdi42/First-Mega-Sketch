ac959881807559a4b1f2940095c49ac81374619b
Jim Cook <jim.cook@nocketts.co.uk>
2018-01-03 17:20:22 +0000

    Initial Commit

---
+++ First Mega Sketch/First_Mega_Sketch.ino
@@ -0,0 +1,66 @@
+//
+// First Mega Sketch
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Jim Cook
+// 				JimCook
+//
+// Date			03/01/2018 17:19
+// Version		<#version#>
+//
+// Copyright	© Jim Cook, 2018
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ First Mega Sketch/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  First Mega Sketch
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project First Mega Sketch
+  Created by Jim Cook on 03/01/2018
+  Copyright © 2018 JimCook
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
