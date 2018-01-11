7b8be9a74f65ed8ffd1981f7af39475f3c0c12f6
Jim Cook <jim.cook@nocketts.co.uk>
2018-01-11 18:56:57 +0000

    This is the basic test of the Mega - flash the light and print hello world

--- First Mega Sketch/First_Mega_Sketch.ino
+++ First Mega Sketch/First_Mega_Sketch.ino
@@ -56,11 +56,17 @@
 // Add setup code
 void setup()
 {
-    ;
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 // Add loop code
 void loop()
 {
-    ;
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(500);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(2000);                       // wait for a second
+    int bytesSent = Serial.println("hello world2");
 }
